3b7961b400da56a84ab1fe99ca63baaa1c0eca06
Jakob <WintersteigerJ190225@sus.htl-grieskirchen.at>
2025-05-15 01:00:44 +0200

    changes

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,6 +3,6 @@ import { useAuthService } from '~/services/authApi';
 export default defineNuxtRouteMiddleware(async (_to, _from) => {
   const result = await useAuthService().validate();
   if (result.isErr()) {
-    return navigateTo('/login');
+    return navigateTo('login');
   }
 });
